8ddf5c77aac77f92750cd5ec00b1ea171c6b7935
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-05-01 20:42:21 +0200

    refactor: extract ToggleTheme cmpnnt from MyApp cmpnnt

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,22 @@ export default function MyApp() {
   return (
     <MyProviders theme={theme} setTheme={setTheme}>
       <WelcomePanel />
-      <label>
-        <input
-          type="checkbox"
-          checked={theme === "dark"}
-          onChange={(e) => {
-            setTheme(e.target.checked ? "dark" : "light");
-          }}
-        />
-        Use dark mode
-      </label>
+      <ToggleTheme  theme={theme} setTheme={setTheme}/>
     </MyProviders>
   );
+}
+
+function ToggleTheme ({theme,setTheme}){
+  return(
+    <label>
+    <input
+      type="checkbox"
+      checked={theme === "dark"}
+      onChange={(e) => {
+        setTheme(e.target.checked ? "dark" : "light");
+      }}
+    />
+    Use dark mode
+  </label>
+  )
 }
